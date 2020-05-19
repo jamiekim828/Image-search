d3682b9ed55378a8ecd08218dde3586be2cac27a
jamiekim828 <jamiekim828@gmail.com>
2020-05-19 18:18:21 +0200

    styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   console.log(props.images);
 
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+    return (
+      <ImageCard key={image.id} image={image} />
+      //   <img alt={images.description} key={images.id} src={images.urls.regular} />
+    );
   });
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
-
-// stephengrider.github.io/playgrounds
