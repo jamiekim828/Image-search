71768184b0a22ec12565d65c4bfd8b8763a7e826
jamiekim828 <jamiekim828@gmail.com>
2020-05-19 12:32:02 +0200

    add axios as dependency and get data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className='ui container' style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+// unsplash API ( unsplash.com/developers )
+// https://api.unsplash.com/
+// GET /search/photos
+// Authorization
+// access key ( RwmHXGt72v7lqBPyHRnAAu-ix4B4lSbBq3b1NubFtyQ )
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID RwmHXGt72v7lqBPyHRnAAu-ix4B4lSbBq3b1NubFtyQ',
+      },
+    });
+  }
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <SearchBar whenSubmitRunThis={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+    this.props.whenSubmitRunThis(this.state.term);
+  };
 
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={(e) => this.onFormSubmit(e)}>
           <div className='field'>
             <label>Image Search</label>
-            <input text='text' onChange={this.onInputChange} />
+            <input
+              type='text'
+              // React prefer Controlled element than Uncontrolled
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
