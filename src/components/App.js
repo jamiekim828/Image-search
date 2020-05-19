7d90d442ac0f6bc51284ddae25d7d3f69f7f404c
jamiekim828 <jamiekim828@gmail.com>
2020-05-19 12:55:20 +0200

    initialized state and setState

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,33 @@ import SearchBar from './SearchBar';
 // access key ( RwmHXGt72v7lqBPyHRnAAu-ix4B4lSbBq3b1NubFtyQ )
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+  // later on we can use map function with array
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID RwmHXGt72v7lqBPyHRnAAu-ix4B4lSbBq3b1NubFtyQ',
       },
     });
-  }
+    //   .then((response) => {
+    //     console.log(response.data.results);
+    //   })
+    console.log(response.data.results);
+    console.log(this);
+
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
-        <SearchBar whenSubmitRunThis={this.onSearchSubmit} />
+        <SearchBar
+          whenSubmitRunThis={this.onSearchSubmit}
+          guessWhatIam='I am props object'
+        />
+        Found: {this.state.images.length} images
       </div>
     );
   }
